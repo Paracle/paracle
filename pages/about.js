87ba894d26ba133234085a12609002470c1e157a
shenkzjay <senksjay@gmail.com>
2023-10-07 17:41:44 +0100

    new files

--- pages/about.js
+++
@@ -1,6 +0,0 @@
-const readMoreBtn = document.querySelector(".readmore-btn");
-const text = document.querySelector(".text");
-
-readMoreBtn.addEventListener("click", (e) => {
-  text.classList.toggle("showmore");
-});
